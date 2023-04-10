8d0c49976cb0c50a007ad131562fdcf2c9c3d69a
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-04-10 16:20:43 +0000

    added the game possibilities and results page

--- Dbaker4876-Final Project.py
+++ Dbaker4876-Final Project.py
@@ -9,6 +9,15 @@
 
 import random
 
+import os
+
+playerWins = 0
+compWins = 0
+
+
+
+path = 'Results.txt'
+
 class Game:
     
     def __init__(self, type):
@@ -28,17 +37,39 @@ def bot():
     if option == 3:
         option = p
         
+def tie():
+    global output
+    print('\nRound {} is a Tie!'.format(x))
+    output += '\nRound {}: Tie'.format(x)
+    
+    
+def playerWin():
+    global output, playerWins
+    print('\n{} wins round {}'.format(player, x))
+    output += '\nRound {}: {} wins'.format(x, player)
+    playerWins += 1
+    
+def botWin():
+    global output, compWins
+    print('\nComputer wins round {}'.format(x))
+    output += '\nRound {}: Computer wins'.format(x)
+    compWins += 1
+        
 print("Welcome to Rock, Paper, Scissors!\n")
 
-player = input("Please enter your name: ")
+player = input("Please enter your name: ").strip().capitalize()
 
-print("Hi {}, you will be playing against the computer. Good luck!".format(player))
+print("Hi {}, you will be playing against the computer. Good luck!\n".format(player))
 
-playerWins = 0
-compWins = 0
 
+output = 'Game between the computer and {}'.format(player)
+
+
+
+x = 1
 while playerWins < 2 or compWins <2:
-    choice = input("Make your choice:\nRock\nPaper\nScissors").strip().lower()
+    bot()
+    choice = input("\nMake your choice:\n\nRock\nPaper\nScissors\n\n").strip().lower()
     while not(choice == 'rock' or choice == 'paper' or choice == 'scissors'):
         choice = input('Please enter a valid option: ').strip().lower()
     if choice == 'rock':
@@ -47,6 +78,42 @@ while playerWins < 2 or compWins <2:
         choice = p
     else:
         choice = s
-    
+    if choice.type == 'rock' and option.type == 'rock':
+        tie()
+    elif choice.type == 'rock' and option.type == 'paper':
+        botWin()
+    elif choice.type == 'rock' and option.type == 'scissors':
+        playerWin()
+    elif choice.type == 'paper' and option.type == 'paper':
+        tie()
+    elif choice.type == 'paper' and option.type == 'rock':
+        playerWin()
+    elif choice.type == 'paper' and option.type == 'scissors':
+        botWin()
+    elif choice.type == 'scissors' and option.type == 'scissors':
+        tie()
+    elif choice.type == 'scissors' and option.type == 'rock':
+        botWin()
+    elif choice.type == 'scissors' and option.type == 'paper':
+        playerWin()
+        
+    x += 1
+    if playerWins == 2:
+        break
+    if compWins == 2:
+        break
+
+if playerWins == 2:
+    output += '\nYou Won!'
+    with open(path, 'w') as file:
+        file.write(output)
+    print('Congratulations! You Win!')
+    print('You can find the results of the match in ' + os.getcwd())
+elif compWins == 2:
+    output += '\nOh No! You lost :('
+    with open(path, 'w') as file:
+        file.write(output)
+    print('Tough luck. You Lost :(')
+    print('You can find the results of the match in ' + os.getcwd())
 
 
